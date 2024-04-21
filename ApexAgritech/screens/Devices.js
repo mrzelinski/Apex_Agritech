8f3a6372cd7dac5d5f5d6e9056d9d22a2fbfd67f
Is03ma <ad2001ismakol@gmail.com>
2024-04-20 21:56:02 -0500

    Put the logo in place in devices, reduced the size of the numbers and letters

--- ApexAgritech/screens/Devices.js
+++ ApexAgritech/screens/Devices.js
@@ -108,7 +108,7 @@ const FrameDeviceLayout = () => {
       </View>
 
       <Image
-        style={[styles.apexTitleIcon, styles.iconPosition]}
+        style={[styles.apexTitleIcon]}
         contentFit="cover"
         source={require("../assets/Title.png")}
       />
@@ -122,41 +122,44 @@ const FrameDeviceLayout = () => {
         activeOpacity={0.2}
         onPress={readData0}
       >
-        <View style={[styles.coop1Child, styles.lineParentPosition]} />
+      
+        <View style={[styles.coop1Child, styles.lineParentPosition]} 
+          // ---------- COOP 1 ---------- //
+        />
         <Text style={[styles.coop11, styles.coopTypo]}>Coop #1</Text>
         <View style={styles.rectangleParent}>
           <View style={[styles.rectangleView, styles.groupChild1Position]} />
           <Text style={[styles.text, styles.textClr]}>{temperatureData0}°</Text>
-          <Text style={[styles.temperature, styles.waterLevelTypo]}>
+          <Text style={[styles.waterLevelTypo]}>
             Temperature
           </Text>
         </View>
         <View style={styles.rectangleGroup}>
           <View style={[styles.groupChild1, styles.groupChild1Position]} />
           <Text style={[styles.text1, styles.textClr]}>{waterLevelData0}</Text>
-          <Text style={[styles.waterLevel, styles.waterLevelTypo]}>
+          <Text style={[styles.waterLevelTypo]}>
             Water Level
           </Text>
         </View>
       </TouchableOpacity>
+
       <TouchableOpacity
         style={[styles.coop2, styles.coopLayout]}
         activeOpacity={0.2}
         onPress={readData1}
       >
-      
         <View style={[styles.coop1Child, styles.lineParentPosition]} />
-        <Text style={[styles.coop21, styles.coopTypo]}>Coop #2</Text>
+        <Text style={[styles.coop21, styles.coopTypo2]}>Coop #2</Text>
         <View style={styles.rectangleContainer}>
           <View style={[styles.rectangleView, styles.groupChild1Position]} />
-          <Text style={[styles.text2, styles.textTypo]}>{waterLevelData1}</Text>
+          <Text style={[styles.textTypo]}>{waterLevelData1}</Text>
           <Text style={[styles.waterLevel1, styles.waterLevel1Typo]}>
             Water Level
           </Text>
         </View>
-        <View style={[styles.groupView, styles.groupViewPosition]}>
+        <View style={[styles.groupViewPosition]}>
           <View style={[styles.rectangleView, styles.groupChild1Position]} />
-          <Text style={[styles.text2, styles.textTypo]}>{temperatureData1}°</Text>
+          <Text style={[styles.textTypo]}>{temperatureData1}°</Text>
           <Text style={[styles.temperature1, styles.waterLevel1Typo]}>
             Temperature
           </Text>
@@ -167,17 +170,17 @@ const FrameDeviceLayout = () => {
         onPress={readData2}
       >
         <View style={[styles.coop1Child, styles.lineParentPosition]} />
-        <Text style={[styles.coop21, styles.coopTypo]}>Coop #3</Text>
-        <View style={styles.rectangleContainer}>
+        <Text style={[styles.coop31, styles.coopTypo3]}>Coop #3</Text>
+        <View style={styles.rectangleContainer2}>
           <View style={[styles.rectangleView, styles.groupChild1Position]} />
-          <Text style={[styles.text2, styles.textTypo]}>{waterLevelData2}</Text>
-          <Text style={[styles.waterLevel2, styles.waterLevel2Typo]}>
+          <Text style={[styles.textTypo2]}>{waterLevelData2}</Text>
+          <Text style={[styles.waterLevel2Typo]}>
             Water Level
           </Text>
         </View>
-        <View style={[styles.rectangleParent2, styles.groupViewPosition]}>
+        <View style={[ styles.groupViewPosition]}>
           <View style={[styles.groupChild1, styles.groupChild1Position]} />
-          <Text style={[styles.text5, styles.textTypo]}>{temperatureData2}°</Text>
+          <Text style={[styles.textTypo2]}>{temperatureData2}°</Text>
           <Text style={[styles.temperature2, styles.waterLevel2Typo]}>
             Temperature
           </Text>
@@ -188,14 +191,14 @@ const FrameDeviceLayout = () => {
         activeOpacity={0.2}
         onPress={() => navigation.navigate('Add a Device')}
         >
-        <Text style={[styles.addNew1, styles.textClr]}>Add New</Text>
+        <Text style={[styles.addNew1, styles.textClrAddNew]}>Add New</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  devicePosition: {
+  devicePosition: {  // whole background (white header, skyblue background)
     width: 430,
     left: 0,
     top: 0,
@@ -212,7 +215,7 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   iconPosition: {
-    overflow: "hidden",
+    overflow: "visible",
     position: "absolute",
   },
   coopLayout: {
@@ -220,39 +223,74 @@ const styles = StyleSheet.create({
     width: 331,
     position: "absolute",
   },
-  lineParentPosition: {
+  lineParentPosition: { // coops 1,2,3 white backgroung
     left: "0%",
     bottom: "0%",
-    top: "0%",
+    top: "-40%",
     height: "100%",
     right: "0%",
     position: "absolute",
     width: "100%",
+    height: "75%",
   },
   coopTypo: { //coop names
-    textAlign: "left",
+    textAlign: "center",
     color: Color.colorDarkorange,
     fontFamily: FontFamily.k2DRegular,
-    fontSize: FontSize.size_21xl,
+    fontSize: FontSize.size_13xl,
     left: "3.32%",
-    height: "31.91%",
+    height: "30.91%",
     position: "absolute",
+    top: -20,
+  },
+
+  coopTypo2: {
+    textAlign: "center",
+    color: Color.colorDarkorange,
+    fontFamily: FontFamily.k2DRegular,
+    fontSize: FontSize.size_13xl,
+    left: "3.32%",
+    height: "30.91%",
+    position: "absolute",
+    top: -20,
+  },
+  coopTypo3:{
+    textAlign: "center",
+    color: Color.colorDarkorange,
+    fontFamily: FontFamily.k2DRegular,
+    fontSize: FontSize.size_13xl,
+    left: "3.32%",
+    height: "30.91%",
+    position: "absolute",
+    top: -20,
   },
   groupChild1Position: { //Temperature and Water level box positions
     backgroundColor: Color.colorAliceblue,
     left: "0%",
     bottom: "0%",
-    top: "0%",
-    height: "100%",
+    top: -70,
+    height: "90%",
     position: "absolute",
   },
-  textClr: {
+  textClr: { // coop 1 temp and water lvl output
     color: Color.colorSteelblue_100,
-    textAlign: "left",
+    textAlign: "center",
+    fontSize: FontSize.size_xl,
+    fontFamily: FontFamily.k2DExtraLight,
+    overflow: "hidden",
+    top: -40,
+  },
+  textClrAddNew: { // Add New text position and size
+    color: Color.colorSteelblue_100,
+    textAlign: "center",
+    fontSize: FontSize.size_xl,
+    fontFamily: FontFamily.k2DExtraLight,
+    overflow: "hidden",
+    top: -160,
   },
   waterLevelTypo: { //  Coop 1 temp and water Text position and size 
     fontSize: FontSize.size_3xs,
-    top: "9.2%",
+    top: -65,
     height: "16.09%",
     left: "12.64%",
     color: Color.colorSteelblue_100,
@@ -261,18 +299,32 @@ const styles = StyleSheet.create({
     textAlign: "left",
     position: "absolute",
   },
-  textTypo: { // Degrees text (coop 2 and 3)
-    bottom: "16.44%",
+  textTypo: { // Degrees text (coop 2)
+    bottom: "96.44%",
+    left: "3%",
+    width: "100%",
     color: Color.colorSteelblue_100,
     fontFamily: FontFamily.k2DExtraLight,
     fontWeight: "200",
-    fontSize: FontSize.size_13xl,
+    fontSize: FontSize.size_5xl,
     height: "47.13%",
     textAlign: "center",
     position: "absolute",
   },
-  waterLevel1Typo: { // Temp and water level (coop 2 and 3)
-    top: "15.34%",
+  textTypo2: { // Degrees text (coop 3)
+    bottom: "96.44%",
+    left: "3%",
+    width: "100%",
+    color: Color.colorSteelblue_100,
+    fontFamily: FontFamily.k2DExtraLight,
+    fontWeight: "200",
+    fontSize: FontSize.size_5xl,
+    height: "47.13%",
+    textAlign: "center",
+    position: "absolute",
+  },
+  waterLevel1Typo: { // Temp and water level labels (coop 2 and 3)
+    top: "-75.00%",
     fontSize: FontSize.size_3xs,
     height: "16.09%",
     color: Color.colorSteelblue_100,
@@ -282,28 +334,31 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   groupViewPosition: { //Coop 2 and 3 temp boxes
-    left: "54.68%",
+    left: "52.68%",
     bottom: "19.15%",
+    width: "22.20%",
     top: "19.15%",
     height: "61.7%",
     position: "absolute",
   },
-  waterLevel2Typo: { // Coop 3 temp and water lvl texts 
-    top: "12.64%",
+  waterLevel2Typo: { // Coop 3 temp and water lvl labels 
+    top: "-72%",
     fontSize: FontSize.size_3xs,
     height: "16.09%",
     color: Color.colorSteelblue_100,
     fontFamily: FontFamily.k2DExtraLight,
     fontWeight: "200",
-    textAlign: "left",
+    textAlign: "center",
     position: "absolute",
+    left: "5%",
+    width: "89.66%",
   },
-  deviceLayourBackChild: { // Background skyblue 3/4
+  deviceLayourBackChild: { // Background skyblue
     backgroundColor: Color.colorLightsteelblue,
     height: 932,
   },
-  deviceLayourBackItem: { // uper side of skyblue background 1/4
-    height: 120,
+  deviceLayourBackItem: { // white header
+    height: 90,
     backgroundColor: Color.colorWhite,
   },
   deviceLayourBack: { //idk
@@ -334,9 +389,9 @@ const styles = StyleSheet.create({
     height: 27,
     top: 43,
   },
-  logoIcon: { // idk
-    top: 788,
-    left: 149,
+  logoIcon: { // logo
+    top: 608,
+    left: 135,
     width: 128,
     height: 103,
   },
@@ -368,7 +423,7 @@ const styles = StyleSheet.create({
     left: "20.72%",
     width: "95.28%",
   },
-  rectangleParent: { //idk yet
+  rectangleParent: { // coop 1 temp lvl blue box
     right: "28.1%",
     left: "52.38%",
     bottom: "22.7%",
@@ -386,16 +441,17 @@ const styles = StyleSheet.create({
     width: "100%",
     fontFamily: FontFamily.k2DExtraLight,
     fontWeight: "200",
-    fontSize: FontSize.size_13xl,
-    bottom: "15.29%",
+    fontSize: FontSize.size_5xl,
+    bottom: "105%",
     height: "47.13%",
     color: Color.colorSteelblue_100,
     position: "absolute",
     textAlign: "center",
   },
-  waterLevel: {
+  waterLevel: { // coop 1 water level text
     width: "88.14%",
     left: "5.08%",
+    top: "105%",
   },
   rectangleGroup: { // coop 1 water level box
     left: "76.13%",
@@ -410,19 +466,30 @@ const styles = StyleSheet.create({
     top: 177,
     left: 30, //53
   },
-  coop21: { // coop 2 and 3 texts (coop #2)
+  coop21: { // coop 2 (coop #2)
     width: "47.43%",
     top: "32.62%",
   },
-  text2: { // celsius symbol coop 2 
-    left: "8.62%",
-    width: "91.38%",
+  coop31:{ // Coop 3 text
+    width:"47.43%",
+    top:"32.62%",
   },
+
   waterLevel1: { // coop 2 water level text
     left: "5%",
     width: "89.66%",
+    top: "9.12%",
   },
-  rectangleContainer: { // coop 2 and 3 Water lvl boxes
+  rectangleContainer: { // coop 2 and 3 Water lvl boxes CHANGE FOR COOP 2 ONLY, CREATE ONE FOR COOP 3, RENAME IT IF NECESSARY
+    left: "76.44%",
+    bottom: "19.15%",
+    top: "19.15%",
+    right: "6.04%",
+    width: "22.52%",
+    height: "61.7%",
+    position: "absolute",
+  },
+  rectangleContainer2: { // coop 2 and 3 Water lvl boxes CHANGE FOR COOP 2 ONLY, CREATE ONE FOR COOP 3, RENAME IT IF NECESSARY
     left: "76.44%",
     bottom: "19.15%",
     top: "19.15%",
@@ -441,17 +508,8 @@ const styles = StyleSheet.create({
     width: "20.52%",
   },
   coop2: { // coop 2 white box (moves everythin inside it)
-    top: 348,
+    top: 310,
     left: 30,
-  },
-  waterLevel2: { // coop 3 water lvl text, somehow
-    left: "12.17%",
-    width: "89.66%",
-  },
-  text5: { // coop 3 temp value, ...
-    left: "5.47%",
-    width: "89.83%",
-    textAlign: "center",
   },
   temperature2: { // coop 3 temp text position
     width: "96.61%",
@@ -463,7 +521,7 @@ const styles = StyleSheet.create({
     left: "54.68%",
   },
   coop3: { // coop 3 white box (moves everythin inside it)
-    top: 519,
+    top: 445,
     left: 30,
   },
   addNew1: { //'add new' text
